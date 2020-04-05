5c4c88ab396dcc04bf3d620c0c891378c77a0f6e
leandrovi <leandrofernandesvieira@gmail.com>
2020-04-05 11:46:14 -0300

    chore: update sql script

--- carpass.sql
+++ carpass.sql
@@ -12,25 +12,22 @@ CREATE TABLE `client`
 PRIMARY KEY (`id`)
 );
 
--- ************************************** `appointment`
+-- ************************************** `vehicle`
 
-CREATE TABLE `appointment`
+CREATE TABLE `vehicle`
 (
- `id`            int unsigned NOT NULL AUTO_INCREMENT ,
- `id_provider`   int unsigned NOT NULL ,
- `id_vehicle`    int unsigned NOT NULL ,
- `vehicle_plate` varchar(7) NOT NULL ,
- `id_specialty`  int unsigned NOT NULL ,
- `date`          datetime NOT NULL ,
- `cancelled_at`  datetime NULL ,
+ `id`        int unsigned NOT NULL AUTO_INCREMENT ,
+ `plate`     varchar(7) NOT NULL ,
+ `id_client` int unsigned NOT NULL ,
+ `year`      int unsigned NOT NULL ,
+ `renavam`   varchar(11) NOT NULL ,
+ `brand`     varchar(45) NOT NULL ,
+ `model`     varchar(45) NOT NULL ,
+ `color`     varchar(45) NOT NULL ,
 
-PRIMARY KEY (`id`),
-KEY `fkIdx_30` (`id_vehicle`, `vehicle_plate`),
-CONSTRAINT `FK_30` FOREIGN KEY `fkIdx_30` (`id_vehicle`, `vehicle_plate`) REFERENCES `vehicle` (`id`, `plate`),
-KEY `fkIdx_40` (`id_provider`),
-CONSTRAINT `FK_40` FOREIGN KEY `fkIdx_40` (`id_provider`) REFERENCES `provider` (`id`),
-KEY `fkIdx_86` (`id_specialty`),
-CONSTRAINT `FK_86` FOREIGN KEY `fkIdx_86` (`id_specialty`) REFERENCES `specialty` (`id`)
+PRIMARY KEY (`id`, `plate`),
+KEY `fkIdx_15` (`id_client`),
+CONSTRAINT `FK_15` FOREIGN KEY `fkIdx_15` (`id_client`) REFERENCES `client` (`id`)
 );
 
 -- ************************************** `plan`
@@ -61,22 +58,14 @@ KEY `fkIdx_82` (`id_plan`),
 CONSTRAINT `FK_82` FOREIGN KEY `fkIdx_82` (`id_plan`) REFERENCES `plan` (`id`)
 );
 
--- ************************************** `rating`
+-- ************************************** `specialty`
 
-CREATE TABLE `rating`
+CREATE TABLE `specialty`
 (
- `id`          int unsigned NOT NULL AUTO_INCREMENT ,
- `id_provider` int unsigned NOT NULL ,
- `id_client`   int unsigned NOT NULL ,
- `score`       int NOT NULL ,
- `comment`     varchar(140) NULL ,
- `created_at`  datetime NOT NULL ,
+ `id`   int unsigned NOT NULL AUTO_INCREMENT ,
+ `name` varchar(60) NOT NULL ,
 
-PRIMARY KEY (`id`),
-KEY `fkIdx_49` (`id_provider`),
-CONSTRAINT `FK_49` FOREIGN KEY `fkIdx_49` (`id_provider`) REFERENCES `provider` (`id`),
-KEY `fkIdx_52` (`id_client`),
-CONSTRAINT `FK_52` FOREIGN KEY `fkIdx_52` (`id_client`) REFERENCES `client` (`id`)
+PRIMARY KEY (`id`)
 );
 
 -- ************************************** `provider_specialty`
@@ -94,39 +83,41 @@ KEY `fkIdx_75` (`id_provider`),
 CONSTRAINT `FK_75` FOREIGN KEY `fkIdx_75` (`id_provider`) REFERENCES `provider` (`id`)
 );
 
--- ************************************** `specialty`
+-- ************************************** `appointment`
 
-CREATE TABLE `specialty`
+CREATE TABLE `appointment`
 (
- `id`   int unsigned NOT NULL AUTO_INCREMENT ,
- `name` varchar(60) NOT NULL ,
+ `id`            int unsigned NOT NULL AUTO_INCREMENT ,
+ `id_provider`   int unsigned NOT NULL ,
+ `id_vehicle`    int unsigned NOT NULL ,
+ `vehicle_plate` varchar(7) NOT NULL ,
+ `id_specialty`  int unsigned NOT NULL ,
+ `date`          datetime NOT NULL ,
+ `cancelled_at`  datetime NULL ,
 
-PRIMARY KEY (`id`)
+PRIMARY KEY (`id`),
+KEY `fkIdx_30` (`id_vehicle`, `vehicle_plate`),
+CONSTRAINT `FK_30` FOREIGN KEY `fkIdx_30` (`id_vehicle`, `vehicle_plate`) REFERENCES `vehicle` (`id`, `plate`),
+KEY `fkIdx_40` (`id_provider`),
+CONSTRAINT `FK_40` FOREIGN KEY `fkIdx_40` (`id_provider`) REFERENCES `provider` (`id`),
+KEY `fkIdx_86` (`id_specialty`),
+CONSTRAINT `FK_86` FOREIGN KEY `fkIdx_86` (`id_specialty`) REFERENCES `specialty` (`id`)
 );
 
--- ************************************** `vehicle`
+-- ************************************** `rating`
 
-CREATE TABLE `vehicle`
+CREATE TABLE `rating`
 (
- `id`        int unsigned NOT NULL AUTO_INCREMENT ,
- `plate`     varchar(7) NOT NULL ,
- `id_client` int unsigned NOT NULL ,
- `year`      int unsigned NOT NULL ,
- `renavam`   varchar(11) NOT NULL ,
- `brand`     varchar(45) NOT NULL ,
- `model`     varchar(45) NOT NULL ,
- `color`     varchar(45) NOT NULL ,
+ `id`          int unsigned NOT NULL AUTO_INCREMENT ,
+ `id_provider` int unsigned NOT NULL ,
+ `id_client`   int unsigned NOT NULL ,
+ `score`       int NOT NULL ,
+ `comment`     varchar(140) NULL ,
+ `created_at`  datetime NOT NULL ,
 
-PRIMARY KEY (`id`, `plate`),
-KEY `fkIdx_15` (`id_client`),
-CONSTRAINT `FK_15` FOREIGN KEY `fkIdx_15` (`id_client`) REFERENCES `client` (`id`)
+PRIMARY KEY (`id`),
+KEY `fkIdx_49` (`id_provider`),
+CONSTRAINT `FK_49` FOREIGN KEY `fkIdx_49` (`id_provider`) REFERENCES `provider` (`id`),
+KEY `fkIdx_52` (`id_client`),
+CONSTRAINT `FK_52` FOREIGN KEY `fkIdx_52` (`id_client`) REFERENCES `client` (`id`)
 );
-
-
-
-
-
-
-
-
-
