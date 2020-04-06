82f24ce324e3c5992d3ed299281ef1c38ccba32b
leandrovi <leandrofernandesvieira@gmail.com>
2020-04-06 17:15:42 -0300

    feat: entity class plan

---
+++ carpass/src/model/entities/Plan.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.entities;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author leandro
+ */
+public class Plan {
+    
+    private Integer id;
+    private String name;
+    private Double value;
+    
+    public Plan() {}
+
+    public Plan(Integer id, String name, Double value) {
+        this.id = id;
+        this.name = name;
+        this.value = value;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 59 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Plan other = (Plan) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Plan{" + "id=" + id + ", name=" + name + ", value=" + value + '}';
+    }
+    
+}
