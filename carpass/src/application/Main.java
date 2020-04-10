dcda56db3160dc1f551f73869bd95a934b213074
leandrovi <leandrofernandesvieira@gmail.com>
2020-04-10 09:10:58 -0300

    refactor: change application type to JavaFX

---
+++ carpass/src/application/Main.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package application;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author leandro
+ */
+public class Main extends Application {
+    
+    private static Scene mainScene;
+    
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/DashboardClient.fxml"));
+            ScrollPane scrollPane = loader.load();
+            
+            scrollPane.setFitToHeight(true);
+            scrollPane.setFitToWidth(true);
+            
+            mainScene = new Scene(scrollPane);
+            
+            primaryStage.setScene(mainScene);
+            primaryStage.setTitle("CarPass");
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static Scene getMainScene() {
+        return mainScene;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ carpass/src/gui/DashboardClientController.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import application.Main;
+import gui.util.Alerts;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+/**
+ *
+ * @author leandro
+ */
+public class DashboardClientController implements Initializable {
+    
+    @FXML
+    private MenuItem menuItemVehicles;
+    
+    @FXML
+    private MenuItem menuItemAppointments;
+    
+    @FXML
+    private MenuItem menuItemProviders;
+    
+    @FXML
+    private MenuItem menuItemAbout;
+
+    @FXML 
+    public void onMenuItemVehiclesAction() {
+        loadView("/gui/VehicleList.fxml");
+    }
+
+    @FXML 
+    public void onMenuItemAppointmentsAction() {
+        System.out.println("onMenuItemAppointmentsAction");
+    }
+
+    @FXML 
+    public void onMenuItemProvidersAction() {
+        System.out.println("onMenuItemProvidersAction");
+    }
+
+    @FXML 
+    public void onMenuItemAboutAction() {
+        loadView("/gui/About.fxml");
+    }
+    
+    @Override
+    public void initialize(URL uri, ResourceBundle rb) { }
+    
+    private synchronized void loadView(String absoluteName) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            VBox newVBox = loader.load();
+            
+            Scene mainScene = Main.getMainScene();
+            VBox dashboardClientVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+            
+            Node dashbaordClientMenu = dashboardClientVBox.getChildren().get(0);
+            dashboardClientVBox.getChildren().clear();
+            dashboardClientVBox.getChildren().add(dashbaordClientMenu);
+            dashboardClientVBox.getChildren().addAll(newVBox.getChildren());
+            
+        } catch (IOException e) {
+            Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+        }
+    }
+    
+}
